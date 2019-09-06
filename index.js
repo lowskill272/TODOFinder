c03cb5ff9e9b5742ff4ebb2b923d10fb3446009c
Dmitry <kambulov.dima455@gmail.com>
2019-09-06 17:24:33 +0300

    pre-release show, important, user {username}

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const { readLine } = require('./console');
 app();
 
 function app () {
-    const files = getFiles();
-
+    const files = getFiles(); 
     console.log('Please, write your command!');
     readLine(processCommand);
 }
@@ -16,6 +15,7 @@ function getFiles () {
 }
 
 function processCommand (command) {
+    reg = /user (.*)/i;
     switch (command) {
         case 'exit':
             process.exit(0);
@@ -26,8 +26,8 @@ function processCommand (command) {
         case 'important':
             important ()
             break;
-        case 'user {username}':
-            user();
+        case reg.exec(command)[0]:
+            user(reg.exec(command)[1]);
             break;
         case 'sort {type}':
             sort();
@@ -42,79 +42,356 @@ function processCommand (command) {
 }
 
 function show (){
-    console.log('show');
     const files = getFiles();
 
-    let reg = /TODO/ig;   
-    const regexp = RegExp('TODO','g');
+    const regexpTODO = /TODO(.*)/ig;
+    const regexpImp = /!/g;
+    const regexpForm = /TODO\s?(.*);\s?(.*);\s?(.*)/i;
+    let output;
+    let imp;
+    let name;
+    let date;
+    let comment;
+    let filename;
+    let delimeter = "  |  ";
+    let space = " "; 
+    let nameMax = 0;
+    let dateMax = 0;
+    let commentMax = 0;
+    let filenameMax = 0;
+    let outputLength = 0;
+    let strArray = [];
 
-    for(let str of files){       
-        while ((matches = regexp.exec(str)) !== null) {
-            console.log(`Found ${matches[0]}. Next starts at ${regexp.lastIndex}.`); 
+    for(let i=0;i<files.length;i++){
+        filename = files[i][0];
+        if(filename.length > filenameMax)
+            filenameMax = filename.length;
+        if(filenameMax > 15)
+            filenameMax = 15;
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){
+            if((result = regexpForm.exec(matches[0])) !== null){
+                name = new String(result[1]);
+                date = new String(result[2]);
+                comment = new String(result[3]);
+                if(name.length > nameMax)
+                    nameMax = name.length;
+                if(nameMax > 10)
+                    nameMax = 10;  
+                if(date.length > dateMax)
+                    dateMax = date.length;
+                if(dateMax > 10)
+                    dateMax = 10;
+                if(comment.length > commentMax)
+                    commentMax = comment.length;
+                if(commentMax > 50)
+                    commentMax = 50;
+            }
         }
-    }    
+    }
+
+    console.log(nameMax, dateMax, commentMax, filenameMax);
+
+    for(let i=0;i<files.length;i++){
+
+        filename = files[i][0];
+
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){            
+
+            if(regexpImp.test(matches[0])){
+                imp = " !";
+            } else{
+                imp = "  ";
+            }
+
+            if((result = regexpForm.exec(matches[0])) !== null){
+                name = new String(result[1]);
+                date = new String(result[2]);
+                comment = new String(result[3]);
+            } else{
+                name = space.repeat(nameMax);
+                date = space.repeat(dateMax);
+                comment = new String(matches[1]);
+            }
+
+            if(name.length > nameMax)
+            name = name.substring(0,7) + "...";     
+        
+            if(name.length < nameMax)
+                name = name + space.repeat(nameMax - name.length);
+                    
+            if(date.length > dateMax)
+                date = date.substring(0,8) + "..."; 
+
+            if(date.length < dateMax)
+                date = date + space.repeat(dateMax - date.length);
+            
+            if(comment.length > commentMax)
+                comment = comment.substring(0,47) + "..."; 
+
+            if(comment.length < commentMax)
+                comment = comment + space.repeat(commentMax - comment.length);
+            
+            output = imp + delimeter + name + delimeter + date + delimeter + comment + delimeter + filename;
+
+            if(output.length > outputLength)
+                outputLength = output.length;
+
+            strArray.push(output);                       
+        }
+    }
+
+    let userHead = 'user' + space.repeat(nameMax - 'user'.length);
+    let dateHead = 'date'+ space.repeat(dateMax - 'date'.length);
+    let commentHead = 'comment' + space.repeat(commentMax - 'comment'.length);
+    let filenameHead = 'fileName' + space.repeat(filenameMax - 'fileName');
+    
+    console.log(" !" + delimeter + userHead + delimeter + dateHead + delimeter + commentHead + delimeter + filenameHead);
+
+    console.log('-'.repeat(outputLength));
+
+    for(let i=0; i<strArray.length; i++){
+        console.log(strArray[i]);
+    }
+
+    console.log('-'.repeat(outputLength));
 }
 
 
-//TODO Отформатировать вывод, разобраться с выводом имени файла, реализовать сортировку и тд.
+//TODO реализовать сортировку и тд.
 
 
 function important (){
     const files = getFiles();
 
-    let reg = /TODO/ig;   
-    const regexpTODO = RegExp('TODO(.*)','ig');
-    const regexpImp = RegExp('!','g');
-    const regexpForm = RegExp('TODO(.*);(.*);(.*)');
+    const regexpTODO = /TODO(.*)/ig;
+    const regexpImp = /!/g;
+    const regexpForm = /TODO\s?(.*);\s?(.*);\s?(.*)/i;
     let output;
+    let imp;
+    let name;
+    let date;
+    let comment;
+    let filename;
+    let delimeter = "  |  ";
+    let space = " "; 
+    let nameMax = 0;
+    let dateMax = 0;
+    let commentMax = 0;
+    let filenameMax = 0;
+    let outputLength = 0;
+    let strArray = [];
 
-    for(let str of files){
-        while ((matches = regexpTODO.exec(str)) !== null){
-            if(regexpImp.exec(matches[0]) !== null){
-                output = "!    |    ";
-            } else{
-                output = "     |    ";
-            }
-
+    for(let i=0;i<files.length;i++){
+        filename = files[i][0];
+        if(filename.length > filenameMax)
+            filenameMax = filename.length;
+        if(filenameMax > 15)
+            filenameMax = 15;
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){
             if((result = regexpForm.exec(matches[0])) !== null){
-                output = output + result[1] + '   |' + result[2] + '   |' + result[3];
-            } else{
-                output = output + '|        |        |     ' + matches[1];
+                name = new String(result[1]);
+                date = new String(result[2]);
+                comment = new String(result[3]);
+                if(name.length > nameMax)
+                    nameMax = name.length;
+                if(nameMax > 10)
+                    nameMax = 10;  
+                if(date.length > dateMax)
+                    dateMax = date.length;
+                if(dateMax > 10)
+                    dateMax = 10;
+                if(comment.length > commentMax)
+                    commentMax = comment.length;
+                if(commentMax > 50)
+                    commentMax = 50;
             }
-
-            console.log(`${output}`);             
         }
     }
 
+    console.log(nameMax, dateMax, commentMax, filenameMax);
 
-    // for(let str of files){
-    //     while ((matches = regexpImp.exec(str)) !== null){
-    //         output = "!    |    ";
-    //         console.log(`${output} ${matches[0]}`);
-    //     }
-    // }
+    for(let i=0;i<files.length;i++){
 
-    // for(let str of files){       
-    //     while ((matches = regexpTODO.exec(str)) !== null) {
-    //         if(regexpImp.exec(matches[0])!== null){
-    //             output = output + "!    |   ";
-    //         }
+        filename = files[i][0];
+
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){            
+
+            if(regexpImp.test(matches[0])){
+                imp = " !";            
+
+                if((result = regexpForm.exec(matches[0])) !== null){
+                    name = new String(result[1]);
+                    date = new String(result[2]);
+                    comment = new String(result[3]);
+                } else{
+                    name = space.repeat(nameMax);
+                    date = space.repeat(dateMax);
+                    comment = new String(matches[1]);
+                }
+
+                if(name.length > nameMax)
+                name = name.substring(0,7) + "...";     
             
-    //         if((result = regexpForm.exec(matches[0]))!== null){
-    //             output = output + '   ' + result[1] + '   |' + result[2] + '   |' + result[3];
-    //         } else {
-    //             output = output + '|        |        |     ';
-    //         }
+                if(name.length < nameMax)
+                    name = name + space.repeat(nameMax - name.length);
+                        
+                if(date.length > dateMax)
+                    date = date.substring(0,8) + "..."; 
 
-    //         console.log(output);
-    //     }
-    // }
+                if(date.length < dateMax)
+                    date = date + space.repeat(dateMax - date.length);
+                
+                if(comment.length > commentMax)
+                    comment = comment.substring(0,47) + "..."; 
+
+                if(comment.length < commentMax)
+                    comment = comment + space.repeat(commentMax - comment.length);
+                
+                output = imp + delimeter + name + delimeter + date + delimeter + comment + delimeter + filename;
+
+                if(output.length > outputLength)
+                    outputLength = output.length;
+
+                strArray.push(output);  
+            }                     
+        }
+    }
+
+    let userHead = 'user' + space.repeat(nameMax - 'user'.length);
+    let dateHead = 'date'+ space.repeat(dateMax - 'date'.length);
+    let commentHead = 'comment' + space.repeat(commentMax - 'comment'.length);
+    let filenameHead = 'fileName' + space.repeat(filenameMax - 'fileName');
+
+    console.log(" !" + delimeter + userHead + delimeter + dateHead + delimeter + commentHead + delimeter + filenameHead);
+
+    console.log('-'.repeat(outputLength));
+
+    for(let i=0; i<strArray.length; i++){
+        console.log(strArray[i]);
+    }
+
+    console.log('-'.repeat(outputLength));
 }
 
 function user (username){
-    console.log('user');
+    const files = getFiles();
 
-}
+    const regexpTODO = /TODO(.*)/ig;
+    const regexpImp = /!/g;
+    const regexpForm = /TODO\s?(.*);\s?(.*);\s?(.*)/i;
+    const regexpName = new RegExp(username, 'i');
+    const regexpPartName = new RegExp('^' + username.substr(0, 2) + '.*', 'i');    
+    // const regexpPartName = new RegExp('asd', 'i'); 
+    let output;
+    let imp;
+    let name;
+    let date;
+    let comment;
+    let filename;
+    let delimeter = "  |  ";
+    let space = " "; 
+    let nameMax = 0;
+    let dateMax = 0;
+    let commentMax = 0;
+    let filenameMax = 0;
+    let outputLength = 0;
+    let strArray = [];
+
+    for(let i=0;i<files.length;i++){
+        filename = files[i][0];
+        if(filename.length > filenameMax)
+            filenameMax = filename.length;
+        if(filenameMax > 15)
+            filenameMax = 15;
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){
+            if((result = regexpForm.exec(matches[0])) !== null){
+                if((regexpName.test(result[1])) || (regexpPartName.test(result[1]))){
+                    name = new String(result[1]);
+                    date = new String(result[2]);
+                    comment = new String(result[3]);
+                    if(name.length > nameMax)
+                        nameMax = name.length;
+                    if(nameMax > 10)
+                        nameMax = 10;  
+                    if(date.length > dateMax)
+                        dateMax = date.length;
+                    if(dateMax > 10)
+                        dateMax = 10;
+                    if(comment.length > commentMax)
+                        commentMax = comment.length;
+                    if(commentMax > 50)
+                        commentMax = 50;
+                }
+            }
+        }
+    }
+
+    console.log(regexpPartName);
+    console.log(nameMax, dateMax, commentMax, filenameMax);
+
+    for(let i=0;i<files.length;i++){
+
+        filename = files[i][0];
+
+        while ((matches = regexpTODO.exec(files[i][1])) !== null){            
+
+            if(regexpImp.test(matches[0])){
+                imp = " !";
+            } else{
+                imp = "  ";
+            }
+
+            if((result = regexpForm.exec(matches[0])) !== null){
+                if((regexpName.test(result[1])) || (regexpPartName.test(result[1]))){
+                    name = new String(result[1]);
+                    date = new String(result[2]);
+                    comment = new String(result[3]);
+
+                    if(name.length > nameMax)
+                    name = name.substring(0,7) + "...";     
+                
+                    if(name.length < nameMax)
+                        name = name + space.repeat(nameMax - name.length);
+                            
+                    if(date.length > dateMax)
+                        date = date.substring(0,8) + "..."; 
+
+                    if(date.length < dateMax)
+                        date = date + space.repeat(dateMax - date.length);
+                    
+                    if(comment.length > commentMax)
+                        comment = comment.substring(0,47) + "..."; 
+
+                    if(comment.length < commentMax)
+                        comment = comment + space.repeat(commentMax - comment.length);
+                    
+                    output = imp + delimeter + name + delimeter + date + delimeter + comment + delimeter + filename;
+
+                    if(output.length > outputLength)
+                        outputLength = output.length;
+
+                    strArray.push(output);    
+                }
+            }                   
+        }
+    }
+
+    let userHead = 'user' + space.repeat(nameMax - 'user'.length);
+    let dateHead = 'date'+ space.repeat(dateMax - 'date'.length);
+    let commentHead = 'comment' + space.repeat(commentMax - 'comment'.length);
+    let filenameHead = 'fileName' + space.repeat(filenameMax - 'fileName');
+    
+    console.log(" !" + delimeter + userHead + delimeter + dateHead + delimeter + commentHead + delimeter + filenameHead);
+
+    console.log('-'.repeat(outputLength));
+
+    for(let i=0; i<strArray.length; i++){
+        console.log(strArray[i]);
+    }
+
+    console.log('-'.repeat(outputLength));
+}                
+               
 
 function sort (type){
     console.log('sort');
